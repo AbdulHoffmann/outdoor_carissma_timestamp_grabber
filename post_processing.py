f7458fb851a57e84f1e8c43a57eae38f230332a6
Abdul Hoffmann <abdul.hoffmann@gmail.com>
2019-05-17 21:27:47 +0200

    venv to pipenv AND added data load functions

--- post_processing.py
+++ post_processing.py
@@ -2,18 +2,54 @@ from __future__ import print_function
 
 import os, sys
 import pandas as pd
-import scipy
+import librosa
+import scipy.io
+from collections import namedtuple
 
 sys.path.insert(0, 'post_processing')
-from load_data import load_csv
+from load_data import load_csv, load_mat
 
 # MY CSV
 csv_name = '1_static_front_10m_ft'
 my_csv_path = os.path.join('acquired_data', 'csv_data')
-
 # MY AUDIOS
+audio_names = ('FL', 'FR', 'BR', 'BL')
+audio_path = os.path.join('acquired_data', 'raw_audios', '1')
+# CARISSMA FILES
+smart_path = os.path.join('acquired_data', 'mat_files', 'Smart')
+Q7_path = os.path.join('acquired_data', 'mat_files', 'Smart')
+mat_name = 'Siren_0001'
 
+# Initializations
+audios = dict()
+sampling_rates = []
 
+# Load data
 df_my_timestamps = load_csv(my_csv_path, csv_name)
+for audio_name in audio_names:
+    audios[audio_name], sr = librosa.load(os.path.join(audio_path, audio_name + '.wav'), sr=None)
+    sampling_rates.append(sr)
+mic_mat, siren_mat = load_mat(
+    mic_path=smart_path,
+    siren_path=Q7_path,
+    mat_name=mat_name
+    )
 
-scipy.io.wavfile.read()
+# Operations
+for name, audio in audios.items():
+    print(name)
+    print(audio.shape)
+
+print(mic_mat['Data1_GPS_Time_msec___Time_in_M'].shape)
+print(mic_mat['Data1_GPS_Time_Week___Start_0h_'].shape)
+print(mic_mat['Data1_INS_Time_msec___Time_in_M'].shape)
+print(mic_mat['Data1_INS_Time_Week___Weeks__St'].shape)
+
+print(siren_mat['Data1_GPS_Time_msec___Time_in_M'].shape)
+print(siren_mat['Data1_GPS_Time_Week___Start_0h_'].shape)
+print(siren_mat['Data1_INS_Time_msec___Time_in_M'].shape)
+print(siren_mat['Data1_INS_Time_Week___Weeks__St'].shape)
+
+# TODO: Crop audio to match my_csv. 
+
+SystemExit
--- post_processing/load_data.py
+++ post_processing/load_data.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
 
 import pandas as pd
+import numpy as np
 import os
+import scipy.io
+import pandas as pd
 
 def load_csv(csv_path, csv_name):
     df_my_recordings = pd.read_csv(os.path.join(csv_path, csv_name + '.csv'), names=['index', 'timestamp'])
@@ -11,9 +14,50 @@ def load_csv(csv_path, csv_name):
         )  # add a single zero so data has the same length
     df_my_recordings['difference'] = df_my_recordings.diff(axis=0)
 
-    print(df_my_recordings, end='\n\n')
+    # print(df_my_recordings, end='\n\n')
 
     print('sampling mean:', round(df_my_recordings['difference'].mean(), 3))
     print('sampling median: ', df_my_recordings['difference'].median())
     return df_my_recordings
-    
+
+def load_mat(mic_path, siren_path, mat_name):
+    mat_mic = scipy.io.loadmat(os.path.join(mic_path, mat_name + '.mat'))
+    mat_siren = scipy.io.loadmat(os.path.join(mic_path, mat_name + '.mat'))
+
+    mat_mic = np.array(
+        [mat_mic['Data1_GPS_Time_msec___Time_in_M'],
+        mat_mic['Data1_GPS_Time_Week___Start_0h_'],
+        mat_mic['Data1_INS_Time_msec___Time_in_M'],
+        mat_mic['Data1_INS_Time_Week___Weeks__St']],
+        dtype={
+            'names':(
+                'Data1_GPS_Time_msec___Time_in_M', 
+                'Data1_GPS_Time_Week___Start_0h_', 
+                'Data1_INS_Time_msec___Time_in_M', 
+                'Data1_INS_Time_Week___Weeks__St'
+            ),
+            'formats':(
+                np.float, np.float, np.float, np.float
+            )
+        }
+    )
+
+    mat_siren = np.array(
+        [mat_siren['Data1_GPS_Time_msec___Time_in_M'],
+        mat_siren['Data1_GPS_Time_Week___Start_0h_'],
+        mat_siren['Data1_INS_Time_msec___Time_in_M'],
+        mat_siren['Data1_INS_Time_Week___Weeks__St']],
+        dtype={
+            'names':(
+                'Data1_GPS_Time_msec___Time_in_M', 
+                'Data1_GPS_Time_Week___Start_0h_', 
+                'Data1_INS_Time_msec___Time_in_M', 
+                'Data1_INS_Time_Week___Weeks__St'
+            ),
+            'formats':(
+                np.float, np.float, np.float, np.float
+            )
+        }
+    )
+
+    return mat_mic, mat_siren
